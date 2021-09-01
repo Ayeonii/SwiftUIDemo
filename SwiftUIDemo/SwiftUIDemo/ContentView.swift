a062ce4cee972642668015f989cda350eb71d132
Ayeon <dldkekdusz@naver.com>
2021-09-01 18:07:16 +0900

    Form, Group, NavigationView 맛보기

--- SwiftUIDemo/SwiftUIDemo/ContentView.swift
+++ SwiftUIDemo/SwiftUIDemo/ContentView.swift
@@ -8,27 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
     
-        List {
-            Button(action : {
-                print("Hello, SwiftUI!!~!")
-            }) {
-                Text("SwiftUI")
-                    .font(.title)
-                    .foregroundColor(.pink)
-            }
-            
-            Text("First")
-            Text("Second")
-            Text("Third")
-            Text("Fourth")
-            Text("Fifth")
-            Text("Sixth")
-            Text("Seventh")
-            Text("Eighth")
-            Text("Ninth")
-        }.font(.largeTitle)
+    //@State -> Struct는 내부 프로퍼티를 변경할 수 없지만, @State 키워드를 붙이면 가능합. Swift 5.1부터 제공.
+    @State var touchedCount : Int = 0
+    var body: some View {
+        NavigationView {
+            Form{
+                Text("Hello World")
+                Text("2Hello World")
+                Text("3Hello World")
+                Text("4Hello World")
+                Text("5Hello World")
+                Text("6Hello World")
+                Text("8Hello World")
+                Text("Button 클릭 횟수 : \(touchedCount)")
+                Button("Button임") {
+                    print("오..신세계..!")
+                    touchedCount += 1
+                }
+                
+                Group {
+                    Text("Group2 Hello World")
+                    Text("Group3 Hello World")
+                    Text("Group4 World")
+                }
+            }.navigationTitle("Navigation 타이틀")
+        }
     }
 }
 
